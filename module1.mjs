18a0680f68df0d93f390336fa2d5bc8183119538
Varad Sunil Jadhav <96735054+VaradJadhav@users.noreply.github.com>
2023-07-14 03:26:16 +0530

    Add files via upload

---
+++ module1.mjs
@@ -0,0 +1,9 @@
+let a = "varad";
+let b = "sam";
+let c = "sunil";
+let d = "sail";
+
+export default a;
+export {b};
+export {c};
+export {d};
---
+++ module2.mjs
@@ -0,0 +1,11 @@
+import dz, { b, c, d } from "./module1.mjs";
+
+console.log(dz);
+console.log(b);
+console.log(c);
+console.log(d);
+
+
+
+// run this by command 
+// node .\module1.mjs
